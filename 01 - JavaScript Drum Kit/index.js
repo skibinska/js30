8912d040a81545d5a2853b288061e6ea4ff4a009
ewelina <skibinska.ewelina@gmail.com>
2017-03-29 23:20:25 +0100

    Fix typo in transform

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,24 +1,22 @@
 (function (window){
   'use strict';
 
-  // const keys = document.querySelectorAll('.key');
-
-  // keys.forEach(key => key.addEventListener('transitionend', removeTransition));
   window.addEventListener('keydown', playSound);
 
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
   function playSound (e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
-    key.addEventListener('transitionend', removeTransition)
   }
 
   function removeTransition (e) {
-    if (e.propertyName !== 'transfrom') return;
-    console.log('removed');
+    if (e.propertyName !== 'transform') return;
     this.classList.remove('playing');
   }
 
